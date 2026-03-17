7ab3b09680f426c36d2b220a15af2439797e034e
sanju-fsd <sanjunathjayapal@gmail.com>
2026-03-17 11:08:43 +0530

    server render

--- client/src/pages/utils/axiosInstance.js
+++ client/src/pages/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-portal-1hxq.onrender.com/api",
 });
 
 // Attach token automatically
